b9abcf71ae4529710bef466e364fbddf48db6bb2
amohamed1 <amohamed@farmdrive.co.ke>
2020-03-20 10:59:28 +0300

    Modification of table data

--- word_scraper_counter.py
+++ word_scraper_counter.py
@@ -5,10 +5,13 @@ from collections import Counter
 import nltk
 import matplotlib.pyplot as plt
 import seaborn as sns
+from prettytable import PrettyTable
 
 words = []
+
+
 # Get the url from a user as input
-url = str(input('Enter a url: '))
+url = str(input('Enter a url and press ENTER: '))
 
 # Get content of the url
 response = requests.get(url)
@@ -19,15 +22,38 @@ defined_regex = '\w+'
 
 tokens = re.findall(defined_regex, soup.text)
 
-print(nltk.pos_tag(soup))
-# for word in tokens:
-#     words.append(word.lower())
-#
-# sns.set()
-#
-# # Create freq dist and plot
-# frequency_dist = nltk.FreqDist(words)
-# frequency_dist.plot(100)
-#
-# plt.show()
-# print(Counter(token))
+# Get a list of all possible english words
+english_words = set(nltk.corpus.words.words())
+
+# Get word type
+words_type = nltk.pos_tag(tokens)
+
+table = PrettyTable(['Word', 'Type'])
+for i in words_type:
+    if i[1] == 'NN':
+        type = 'Noun'
+    elif i[1] == 'VBP':
+        type == 'Verb'
+    else:
+        type == 'other'
+    table.add_row([i[0], type])
+
+print(table)
+for word in tokens:
+    if word in english_words:
+        words.append(word.lower())
+
+# Get word count
+word_count = Counter(words)
+
+# Get most common 100 words
+most_common_100_words = word_count.most_common(100)
+sns.set()
+
+# Create freq dist
+frequency_dist = nltk.FreqDist(words)
+
+# Plot the graph
+frequency_dist.plot(100)
+
+plt.show()
